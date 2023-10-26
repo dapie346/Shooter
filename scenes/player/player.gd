00727f878c87bf5c47651637fddbdad985aa31e4
dapie346 <dawid.pietruszewski@gmail.com>
2023-10-26 19:19:46 +0200

    Added first object, movable player character and collisions

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,1 +1,13 @@
-extends Node2D
+extends CharacterBody2D
+
+func _process(delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * 500
+	move_and_slide()
+	
+	if Input.is_action_pressed("primary_action"):
+		print('shoot')
+		
+	if Input.is_action_pressed("secondary_action"):
+		print('grenade')
+		
