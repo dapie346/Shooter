7bd041ee97daa82116e5e4173ef582076b2db4e2
dapie346 <dawid.pietruszewski@gmail.com>
2023-10-28 22:08:51 +0200

    added projectiles for shooting and grenades

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,4 +1,18 @@
 extends Node2D
 
-func _on_area_2d_body_entered(body):
-	print("body has entered")
+var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
+var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
+
+func _on_gate_player_entered_gate(body):
+	if body == $Player:
+		print("player has entered")
+
+func _on_player_laser(pos):
+	var laser = laser_scene.instantiate()
+	laser.position = pos
+	$Projectiles.add_child(laser)
+
+func _on_player_grenade(pos):
+	var grenade = grenade_scene.instantiate()
+	grenade.position = pos
+	$Projectiles.add_child(grenade)
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
-@export var speed = 500
+@export var speed: int = 500
 var can_laser: bool = true
 var can_grenade: bool = true
+
+signal laser
+signal grenade
 
 func _process(_delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
@@ -10,13 +13,18 @@ func _process(_delta):
 	move_and_slide()
 	
 	if Input.is_action_pressed("primary_action") and can_laser:
-		print('shoot')
+		var laser_markers = $LaserStartPositions.get_children()
+		var selected_laser = laser_markers[randi() % laser_markers.size()]
 		can_laser = false
+		$LaserTimer.start()
+		laser.emit(selected_laser.global_position)
 		
 	if Input.is_action_pressed("secondary_action") and can_grenade:
-		print('grenade')
+		var grenade_markers = $GrenadeStartPositions.get_children()
+		var selected_grenade = grenade_markers[randi() % grenade_markers.size()]
 		can_grenade = false
-		
+		$GrenadeTimer.start()
+		grenade.emit(selected_grenade.global_position)
 
 
 func _on_laser_timer_timeout():
